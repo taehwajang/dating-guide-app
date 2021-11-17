0de8c3ddfd10c6b1d1a3c3f1935b87b7f40cfbb5
taehwajang <61370482+taehwajang@users.noreply.github.com>
2021-11-17 18:28:53 +0900

    title change: Dating Guide

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({Component, pageProps}) {
   return ( 
     <>
       <Head>
-        <title>My page title</title>
+        <title>Dating Guide</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=857f1bc969bb444d47a2154d19375d92"></script>
         <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=857f1bc969bb444d47a2154d19375d92&libraries=LIBRARY"></script>
